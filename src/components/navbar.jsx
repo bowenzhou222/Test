382eb7ecf2b10f3424c3921c3af045dfd9850c57
bowenzhou222 <bowenzhou222@gmail.com>
2018-04-21 14:34:53 +1000

    Navbar fit to small screens

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,56 @@
 import React from 'react';
 import logo from '../assets/images/logo.png';
+import CustomerFeedback from './customer_feedback/customer_feedback_container';
 
-const Navbar = () => (
-    <div className="sticky-navbar">
-        <a href="https://www.cammy.com/au/">
-            <img src={logo} className="cammy-logo" />
-            Cammy
-          </a>
-        <div className="navbar-help-links">
-            <a href="https://www.cammy.com/au/how-it-works/">
-                How it works
-            </a>
-            <a href="https://www.cammy.com/au/cameras/">
-                Cameras
-            </a>
-            <a href="https://www.cammy.com/au/apps/">
-                Apps
-            </a>
-            <a href="https://au.checkout.cammy.com/">
-                Cart
-            </a>
-            <a href="tel:1300652430">
-                1 300 652 430
-            </a>
-        </div>
-    </div>
-);
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavbarOpen: false,
+        };
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+    }
+
+    toggleNavbar() {
+        const { isNavbarOpen } = this.state;
+        this.setState({ isNavbarOpen: !isNavbarOpen });
+    }
+
+    render() {
+        const { isNavbarOpen } = this.state;
+
+        return (
+            <div className="sticky-navbar">
+                <a href="https://www.cammy.com/au/">
+                    <img src={logo} className="cammy-logo" />
+                    Cammy
+                </a>
+                <div className={`navbar-help-links ${!isNavbarOpen ? 'display-none' : ''}`}>
+                    <a href="https://www.cammy.com/au/how-it-works/" onClick={this.toggleNavbar}>
+                        How it works
+                    </a>
+                    <a href="https://www.cammy.com/au/cameras/" onClick={this.toggleNavbar}>
+                        Cameras
+                    </a>
+                    <a href="https://www.cammy.com/au/apps/" onClick={this.toggleNavbar}>
+                        Apps
+                    </a>
+                    <a href="https://au.checkout.cammy.com/" onClick={this.toggleNavbar}>
+                        Cart
+                    </a>
+                    <a href="tel:1300652430" onClick={this.toggleNavbar}>
+                        1 300 652 430
+                    </a>
+                </div>
+
+                <div className="navbar-toggle-button" onClick={this.toggleNavbar}>
+                    MENU
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Navbar;
